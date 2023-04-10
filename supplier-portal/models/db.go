b36bf4a15b6f2e50d88ac42765d3dfd7ce8c9a0d
gauravpcu <gaurav.kumar@procurementpartners.com>
2023-04-10 14:36:39 -0400

    changes

--- supplier-portal/models/db.go
+++ supplier-portal/models/db.go
@@ -11,7 +11,11 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	dsn := "root:Procurement!2023@tcp(127.0.0.1:3306)/sp_test?charset=utf8mb4&parseTime=True&loc=Local"
+	//dsn := "root:Procurement!2023@tcp(127.0.0.1:3306)/sp_test?charset=utf8mb4&parseTime=True&loc=Local"
+	//server=benchmarking.mysql.database.azure.com;database=sp_test;user=sproot;password=Procurement!2023
+
+	dsn := "sproot:Procurement!2023@tcp(benchmarking.mysql.database.azure.com)/sp_test?charset=utf8mb4&parseTime=True&loc=Local"
+
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
